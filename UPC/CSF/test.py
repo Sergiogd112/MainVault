bceee89855b2fe64c118c61555725780fc16581b
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-06-15 16:34:29 +0200

    Update test.py

--- UPC/CSF/test.py
+++ UPC/CSF/test.py
@@ -1,72 +1,49 @@
+import sympy as sp
+from scipy.special import erfcinv
 import numpy as np
 
-r = [
-    0.8 - 0.2j,
-    -0.1 + 0.6j,
-    0.3 + 0.6j,
-    0.8 - 0.2j,
-    -0.3 + 0.1j,
-    -0.03 - 0.1j,
-    -0.6 - 1j,
-    -0.3 + 0.1j,
-    0.3 + 0.04j,
-    0.1 - 0.6j,
-    -0.5 - 0.8j,
-    0.3 + 0.04j,
-    -0.3 + 0.1j,
-    -0.03 - 0.1j,
-    -0.6 - 1j,
-    -0.3 + 0.1j,
-    -0.8 + 0.2j,
-    0.1 - 0.6j,
-    -0.3 - 0.6j,
-    -0.8 + 0.2j,
-    0.8 - 0.2j,
-    -0.1 + 0.6j,
-    0.3 + 0.6j,
-    0.8 - 0.2j,
-]
+# Assign the other parameters defined in the excercisse
+# Define the symbols
+No, Eb, M, Gp, SIR, Tb, Tc, d, blob = sp.symbols("N_o,E_b,M,G_p,SIR,T_b,T_c,d,blob")
+# define the BER
+BER = 0.5 * sp.erfc(blob)
 
-y = [
-    0.35 + 0.35j,
-    -0.5 + 0j,
-    0 + 0.25j,
-    -0.35 - 0.35j,
-    0.5 - 0j,
-    -0.17 + 0.17j,
-    0 - 0.5j,
-    0.35 - 0.35j,
-    -0.17 + 0.17j,
-    -0.35 - 0.35j,
-    0.5 - 0j,
-    -0.17 + 0.17j,
-    -0.35 - 0.35j,
-    0.5 + 0j,
-    -0.25j,
-    0.35 + 0.35j,
-    -0.5 + 0j,
-    0 + 0.25j,
-]
-
-rb = 24e6
-
-k = 0
-for i in range(1, len(r)):
-    if r[i] == r[0]:
-        for j in range(i + 1, len(r)):
-            if r[j] != r[j - i]:
-                k = j
-                break
-        break
-
-N = i
-M = k
-
-nsyms = 16
-nbits = np.log2(nsyms)
-rs_eff = rb / nbits
-print(f"rs={rs_eff}")
-rs = rs_eff / (N / M)
-print(f"rs_eff={rs}")
-Bt = (N + 1) * rs / N
-print(f"Bt={Bt}")
+BER.simplify()
+# set the BER definition to equal 3.5e-3
+eq = BER - 12e-3
+# Find the value of the inner part of the erfc
+res = sp.solve(eq, blob)
+inerfc = erfcinv(float(sp.erfc(res[0])))
+a = 1 / inerfc**2
+beta = 0.15
+BW = 184e6
+vals = {
+    No: 5e-18,
+}
+r = 1100
+L = 25 + 32 * sp.log(r, 10)
+D = 12
+Pt_dBm = 30
+PrdBm = Pt_dBm - L + D
+PrW = 10 ** ((PrdBm - 30) / 10)
+act = 0.4
+rb = 400e3
+Tb = 1 / rb
+Eb = PrW * Tb
+# state the equations for Gp=Tb/Tc and BW=(1+beta)/Tc
+eq2 = Gp - Tb / Tc
+eq3 = (1 + beta) / Tc - BW
+# state the equation for the inner part of the BER formula
+eq = No / Eb + (M - 1) / Gp - a
+# solve for all the unknowns
+res = sp.solve([eq.subs(vals), eq2, eq3])[0]
+# print the results
+print(res)
+print("Gp:", res[Gp])
+print("M:", res[M])
+print("Tc:", res[Tc])
+# Compute the density of user/km^2
+Mprim = np.floor(res[M] / act)
+rhokm2 = np.floor(Mprim / (3.14159264 * (r / 1000) ** 2))
+print("M':", Mprim)
+print("density:", rhokm2, "users/km^2")
